865fb5218801a19f92cf3423a71d38549c1251af
thenfdi <thenfdg@gmail.com>
2024-04-05 17:06:20 +0200

    EnsambladorArchivosa

--- EnsambladorArchivos.java
+++ EnsambladorArchivos.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class EnsambladorArchivosa {
+public class EnsambladorArchivos {
     private String ubicacionArchivos;
 
     public EnsambladorArchivos(String ubicacionArchivos) {
